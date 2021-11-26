358fa5b8cda0adbf1bcae8c6fe342664a09d2b5b
Tallahasseeee <90040962+Tallahasseeee@users.noreply.github.com>
2021-11-26 22:42:44 +0200

    Add files via upload

---
+++ Lab7/Lab7.py
@@ -0,0 +1,27 @@
+N = int(input("Enter n: "))
+def Input(n):
+    A = [0] * n
+    for i in range(0, n):
+        A[i] = float(input())
+    return A
+
+
+def process(*args):
+    A = args[0]
+    n = args[-1]
+    min = A[0]
+    max = A[0]
+    min_index = 0
+    max_index = 0
+    for i in range(0, n):
+        if A[i] > max:
+            max = A[i]
+            max_index = i
+        elif A[i] < min:
+            min = A[i]
+            min_index = i
+    A[max_index] = min
+    A[min_index] = max
+    return A
+
+print(process(Input(N), N))
